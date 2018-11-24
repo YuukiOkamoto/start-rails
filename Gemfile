205b0c2c296e259a4efd74219f25e674d26dc39b
okamotoyuuki <yuuki.okamoto.it.fitness@gmail.com>
2018-11-24 12:36:22 +0900

    テンプレートファイルをslimに変更

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.5'
 gem 'rails-i18n', '~> 5.1'
+gem 'slim-rails'
 # gem 'turbolinks'
 
 # Storage
--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module StartRails
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
+    config.generators.template_engine = :slim
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
